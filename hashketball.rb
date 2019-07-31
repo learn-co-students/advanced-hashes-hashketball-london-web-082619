ac40a86fff16aa2095e15bb35748f0c00ceb9fdb
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-07-31 18:21:38 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,68 @@
-# Write your code here!
+require "pry"
+def game_hash
+  {:home => {:team_name => "Broklyn Nets", :colors => ["Black", "White"], 
+  :players => 
+  ["Alan Anderson"=> {:number => "0", :shoe => "16", :points => "22", :rebounds => "12", :assists => "12", :steals => "3", :blocks => "1", :slam_dunks => "1"},
+    "Reggie Ewans" => {:number => "30", :shoe => "14", :points => "12", :rebounds => "12", :assists => "12", :steals => "12", :blocks => "12", :slam_dunks => "7" }, 
+    "Brook Lopez" => {:number => "17", :shoe => "10", :points => "12", :rebounds => "19", :assists =>"10", :steals => "3", :blocks => "1", :slam_dunks => "15" },
+    "Mason Plumlee" => {:number => "1", :shoe => "19", :points =>"26", :rebounds => "11", :assists => "6", :steals => "3", :blocks => "8", :slam_dunks => "5" }, 
+    "Jason Terry" => {:number => "31", :shoe => "15", :points => "19", :rebounds => "2", :assists => "2", :steals => "4", :blocks => "11", :slam_dunks => "1" }]
+    
+    },
+    :away => {:team_name => "Charlote Hornets", :colors =>["Turquoise", "Purple"], :players =>
+    ["Jeff Adrien" => {:number => "4", :shoe => "18", :points =>"10", :rebounds => "1", :assists =>"1", :steals => "2", :blocks => "7", :slam_dunks => "2"},
+    "Bismack Biyombo"=> {:number => "0", :shoe => "16", :points =>"12", :rebounds => "4", :assists =>"7", :steals => "22", :blocks => "15", :slam_dunks => "10"},
+    "DeSagna Diop" => {:number => "2", :shoe => "14", :points => "24", :rebounds => "12", :assists => "12", :steals => "4", :blocks => "5", :slam_dunks => "5"},
+    "Ben Gordon" => {:number => "8", :shoe => "15", :points =>"33", :rebounds => "3", :assists =>"2", :steals => "1", :blocks => "1", :slam_dunks => "0"},
+    "Kemba Walker" =>{:number => "33", :shoe => "15", :points =>"6", :rebounds => "12", :assists =>"12", :steals => "7", :blocks => "5", :slam_dunks => "12"}]
+  }}
+end
 
 
 
+#1 iterates over home and team
+#2itertaes over players and player names
+#3 iterates over player name and its information
+# if parameter is equal to player name
+# return that player scored points.
+def num_points_scored (sportsman)
+game_hash
+scores = 0
+  game_hash.each do |team_position, team_data|
+  team_data.each do | player_name, information|
+  if player_name == :players
+  information.each do |name, numbers|
+  if name == sportsman
+  numbers.each do |key, value|
+  if key == :points
+  scores = value.to_i
+  binding.pry
+  end
+  end
+  end
+  end
+  end
+  end
+  end 
+   return scores
+  end
+ def shoe_size (player_name)
+   game_hash.each do |team, atributes|
+atributes[:players].each do |member, stats|
+if player_name == member
+ return stats[:shoe].to_i
+end
+end
+end
+end
+
+def team_colors (team_name)
+game_hash.each do |team, atributes|
+if team == team_name
+return atributes[:colors]
+end
+end
+end
 
 
 
